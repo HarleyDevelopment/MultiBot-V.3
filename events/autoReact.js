8073ef395f860d6fb1fbac2ba7519dca61e63514
Harley <92220233+HarleyDevelopment@users.noreply.github.com>
2021-10-09 16:18:19 +0100

    Update autoReact.js

--- events/autoReact.js
+++ events/autoReact.js
@@ -20,6 +20,8 @@ module.exports = {
             const channel = client.channels.cache.get(client.config.autoReactChannel)
             if(!channel) return console.log(chalk.red(`You have auto-react enabled, but don't have a channel set! For me to work, please set a channel`))
 
+            if(message.deleted) return;
+            
             if(message.channel.id == channel) {
                 if(client.config.botBypassAutoRole == true) {
                     if(message.author.bot) return;
